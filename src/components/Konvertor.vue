810a19984d7d9b16400db9072643c7b1b68c215c
MarkoKillin <mirkovicmarko790@gmail.com>
2021-11-27 18:55:27 +0100

    Konverter v2.0

--- src/components/Konvertor.vue
+++ src/components/Konvertor.vue
@@ -36,7 +36,7 @@ export default {
     data(){
         return{
             api: '',
-            unos: '',
+            unos: '0',
             vrednost: '',
             pvaluta: 'RSD',
             dvaluta: 'EUR',
